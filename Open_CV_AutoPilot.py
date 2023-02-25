98a2737379a17220561a02fb2316f1ffd5eae2dd
GunterDeQuark <57863558+GunterDeQuark@users.noreply.github.com>
2023-02-25 04:31:49 +0300

    Update Method

--- Open_CV_AutoPilot.py
+++ Open_CV_AutoPilot.py
@@ -1,29 +1,47 @@
-
 import numpy as np
-from PIL import ImageGrab, Image
+from PIL import ImageGrab
 import cv2
 import time
+from directkeys import ReleaseKey, PressKey, W, A, S, D 
+import lanes
 
-def process_img(image):
-    original_image = image
-    #convert to gray
-    processed_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #edge detection
-    processed_img = cv2.Canny(processed_img, threshold1 = 200, threshold2 = 300)
-    return processed_img
 
-#def main():
-while(True):
+def process_img(original_image):
+    try:
+        processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
+        copy_img = np.copy(processed_img)
+        processed_img = cv2.GaussianBlur(processed_img,(5,5),0)
+        processed_img = cv2.Canny(processed_img, threshold1=50, threshold2=150) #1 to 2 or 1 to 3
+        #mask 
+        vertices = np.array([[100,500],[100,300],[300,300],[500,300],[800,300],[800,500],
+                             ], np.int32)
+
+        #processed_img = lanes.roi(processed_img, [vertices])
+        processed_img = lanes.region_of_interest(processed_img)
+        lines = cv2.HoughLinesP(processed_img, 2, np.pi/180, 100, np.array([]), 20, 5)
+        averaged_lines = lanes.average_slope_intercept(processed_img, lines)
+        line_image = lanes.display_lines(copy_img, averaged_lines)
+
+        combo = cv2.addWeighted(copy_img, 0.8, line_image, 0.5, 1)
+        return combo#processed_img
+    except:
+        return original_image
+
+
+
+
+
+def main():
     last_time = time.time()
     while(True):
-        # 800x600 windowed mode
-        screen = np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-        #print('loop took {} deconds'.format(time.time()-last_time))
-        last_time = time.time()
+        screen =  np.array(ImageGrab.grab(bbox=(0,40, 800, 640)))
         new_screen = process_img(screen)
+        print('Loop took {} seconds'.format(time.time()-last_time))
+        last_time = time.time()
         cv2.imshow('window', new_screen)
-        #cv2.imshow('window', cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        #cv2.imshow('window2', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
 
+main()
---
+++ lanes.py
@@ -0,0 +1,60 @@
+import cv2
+import numpy as np
+
+
+
+
+def make_coordinates(image, line_parameters):
+    # Y = MX + B
+    slope, intercept = line_parameters
+    y1 = image.shape[0]
+    y2 = int(y1 * (3/5))
+    x1 = int((y1 - intercept) / slope)
+    x2 = int((y2 - intercept) / slope)
+    return np.array([x1, y1, x2, y2])
+
+
+def average_slope_intercept(image, lines):
+    left_fit = []
+    right_fit = []
+    while lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            parameters = np.polyfit((x1, x2), (y1, y2), 1)
+            slope = parameters[0]
+            intercept = parameters[1]
+            if slope < 0:
+                left_fit.append((slope, intercept))
+            else:
+                right_fit.append((slope, intercept))
+        left_fit_average = np.average(left_fit, axis=0)
+        left_line = make_coordinates(image, left_fit_average)
+        right_fit_average = np.average(right_fit, axis=0)
+        right_line = make_coordinates(image, right_fit_average)
+        return np.array([left_line, right_line])
+
+
+def display_lines(image, lines):
+    line_image = np.zeros_like(image)
+    if lines is not None:
+        for x1, y1, x2, y2 in lines:
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 8)
+    return line_image
+
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([(350, height/2), (800-350, height/2), (790, 450), (0, 450)])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, np.array([polygons], dtype=np.int64), 1024)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+def roi(img, vertices):
+    #blank mask:
+    mask = np.zeros_like(img)
+    # fill the mask
+    cv2.fillPoly(mask, vertices, 255)
+    # now only show the area that is the mask
+    masked = cv2.bitwise_and(img, mask)
+    return masked
